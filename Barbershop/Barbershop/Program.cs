f5f32df1ddc01d9f0348a178653bf67afd6f27c9
Cristian-Madalin Cristian <cazanmadalin12@gmail.com>
2025-12-05 22:25:00 +0200

    New Project

---
+++ Barbershop/Barbershop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Brabershop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Welcome to the Barbershop!");
+        }
+    }
+}
